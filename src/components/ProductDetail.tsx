92e63530b015b280e35cfa573c3ef6e7f4be2d11
nico-villalonga <nicovillalonga90@gmail.com>
2024-07-07 10:40:17 -0300

    feature: Add ProductDetail component

---
+++ src/components/ProductDetail.tsx
@@ -0,0 +1,96 @@
+import type { Product } from "@/types"
+import { formatReviewDate } from "@/utils/date"
+import {
+  getProductPrice,
+  getProductReviews,
+  getProductTags,
+  getRatingAsStars,
+} from "@/utils/product"
+
+export function ProductDetail(product: Product) {
+  return (
+    <div className="w-full mx-auto p-10">
+      <div className="bg-white">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10 bg-white max-w-5xl min-w-96 p-8 mx-auto">
+          <div className="h-full max-lg:mx-auto">
+            <img
+              src={product.images[0]}
+              className="max-lg:mx-auto lg:ml-auto h-full"
+            />
+          </div>
+          <div className="felx max-w-96 justify-self-center md:justify-self-auto xl:justify-start justify-center items-center content-center">
+            <div className="flex flex-col w-full max-w-xl gap-4">
+              <div className="flex gap-2">{getProductTags(product)}</div>
+              <div>
+                <h1 className="text-xl leading-none">{product.title}</h1>
+                <h2 className="text-sm">{product.brand}</h2>
+              </div>
+              <div className="flex flex-col flex-wrap">
+                {getProductPrice(product)}
+                {getProductReviews(product)}
+              </div>
+              <div className="text-xs">
+                <p className="text-sm">
+                  {product.stock <= 10 && (
+                    <span className="text-red-400 font-bold pr-2">
+                      {product.availabilityStatus}
+                    </span>
+                  )}
+                  Available <span className="font-bold">{product.stock}</span>
+                </p>
+                <p>{product.shippingInformation}</p>
+                <p className="text-neutral-400">{product.returnPolicy}</p>
+              </div>
+
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+                <button className="group py-4 px-5 rounded-md bg-indigo-50 text-indigo-600 font-semibold text-lg w-full flex items-center justify-center gap-2 transition-all duration-500 hover:bg-indigo-100">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-8">
+          <section className="p-8 before:border-t before:border-solid before:border-neutral-200 before:content-[''] before:block before:h-px before:w-10/12 before:my-10 before:mx-auto">
+            <h3 className="text-lg font-semibold">Specifications:</h3>
+            <div className="flex flex-col gap-1 pt-4">
+              <p>weight: {product.weight}</p>
+              <p>
+                dimensions:{" "}
+                {`${product.dimensions.height}" x ${product.dimensions.width}" x ${product.dimensions.depth}"`}
+              </p>
+            </div>
+          </section>
+
+          <section className="p-8 before:border-t before:border-solid before:border-neutral-200 before:content-[''] before:block before:h-px before:w-10/12 before:my-10 before:mx-auto">
+            <h3 className="text-lg font-semibold">Description:</h3>
+            <p className="pt-4">{product.description}</p>
+          </section>
+
+          <section className="p-8 before:border-t before:border-solid before:border-neutral-200 before:content-[''] before:block before:h-px before:w-10/12 before:my-10 before:mx-auto">
+            <h3 className="text-lg font-semibold">Reviews:</h3>
+            <div className="flex flex-col gap-8 pt-4 items-center">
+              {product.reviews.map((review, i) => (
+                <div key={i}>
+                  <div className="flex flex-col items-center">
+                    {getRatingAsStars(review.rating)}
+                  </div>
+                  <p>{review.comment}</p>
+                  <div className="pt-2 text-center">
+                    <p className="text-sm text-neutral-500 italic font-medium">
+                      {review.reviewerName}
+                    </p>
+                    <p className="text-xs text-neutral-400 italic font-medium">
+                      {formatReviewDate(review.date)}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -3,11 +3,11 @@ import { getProductPrice, getProductReviews } from "@/utils/product"
 
 export function ProductPreview(product: Product) {
   return (
-    <article className="flex flex-col sm:flex-row gap-8 md:gap-6 border border-neutral-400 p-10 bg-white max-w-2xl">
+    <article className="flex flex-col sm:flex-row gap-8 md:gap-6 p-10 bg-white max-w-2xl">
       <div className="flex justify-center">
         <div className="size-40">
           <img
-            src="https://pagedone.io/asset/uploads/1700471600.png"
+            src={product.thumbnail}
             alt={product.title}
             className="m-auto h-full"
           />
@@ -15,7 +15,7 @@ export function ProductPreview(product: Product) {
       </div>
       <div className="w-full h-full xl:justify-start justify-center flex items-center">
         <div className="flex flex-col gap-4 w-full h-full max-w-xl">
-          <div className="">
+          <div>
             <h3 className="text-xl leading-none">{product.title}</h3>
             <h4 className="text-sm">{product.brand}</h4>
           </div>
@@ -29,14 +29,17 @@ export function ProductPreview(product: Product) {
                 {product.availabilityStatus}
               </p>
             )}
-            <p>{product.shippingInformation}</p>
-            <p className="text-neutral-400 text-xs">{product.returnPolicy}</p>
           </div>
 
-          <div className="w-full sm:w-32 h-8">
-            <button className="py-4 px-5 rounded-full bg-indigo-100 text-indigo-600 font-semibold text-md w-full h-full flex items-center justify-center gap-2 transition-all duration-500 hover:bg-indigo-200">
-              Add to cart
-            </button>
+          <div>
+            <p className="text-xs text-neutral-400">
+              {product.shippingInformation}
+            </p>
+            <div className="w-full sm:w-32 h-8">
+              <button className="py-4 px-5 rounded-md bg-indigo-100 text-indigo-600 font-semibold text-md w-full h-full flex items-center justify-center gap-2 transition-all duration-500 hover:bg-indigo-200">
+                Add to cart
+              </button>
+            </div>
           </div>
         </div>
       </div>
