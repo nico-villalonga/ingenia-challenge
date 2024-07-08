ca4a646a9d839237ae57aaac816fe914cdae6abc
nico-villalonga <nicovillalonga90@gmail.com>
2024-07-08 01:11:34 -0300

    feature: Add products api

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,24 +1,19 @@
 "use client"
 
-import { ProductPreview } from "./ProductPreview"
-import { useEffect, useState } from "react"
-import type { Product } from "@/types"
+import { useGetAllProductsQuery } from "@/store/api/products.api"
+import ProductPreview from "./ProductPreview"
 
 export const Products = () => {
-  const [products, setProducts] = useState<Product[]>([])
+  const { data, isError, isLoading } = useGetAllProductsQuery({
+    limit: 2,
+    skip: 0,
+  })
 
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data.products))
-      .catch((err) => {
-        console.error("Error fetching products:", err)
-      })
-  }, [])
+  if (!data?.products.length) return
 
   return (
     <div className="flex flex-col gap-2 mx-auto">
-      {products.map((product) => {
+      {data.products.map((product) => {
         return <ProductPreview key={product.id} {...product} />
       })}
     </div>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,17 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import { productsApiSlice } from "./api/products.api"
 
-const rootReducer = combineSlices()
+const rootReducer = combineSlices(productsApiSlice)
 export type RootState = ReturnType<typeof rootReducer>
 
 export const makeStore = () => {
   return configureStore({
     reducer: rootReducer,
+    // Adding the api middleware enables caching, invalidation, polling from `rtk-query`.
+    middleware: (getDefaultMiddleware) => {
+      return getDefaultMiddleware().concat(productsApiSlice.middleware)
+    },
   })
 }
 
